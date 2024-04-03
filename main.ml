71b59b72556757aab01778fdaa6a62074388f410
zcw <zcw@zcw.local>
2024-04-03 09:57:46 +0200

    use full module namespace (e.g Peano.( + ) instead of just '+'

--- main.ml
+++ main.ml
@@ -1,26 +1,30 @@
 open Parser
-open Peano
-
 
 let rec eval (ast:Parser.ast) : Peano.p =
-    let open Parser in
-    let open Peano in
     match ast with 
-    | Const i -> Peano.n i
-    | Add (x,y) -> eval x + eval y
-    | Sub (x,y) -> eval x - eval y
-    | Mod (x,y) -> (eval x) % (eval y)
-    | Mul (x,y) -> eval x * eval y
-    | Div (x,y) -> eval x / eval y
-    | Exp (x,y) -> eval x ^ eval y
+    | Parser.Const i -> Peano.n i
+    | Parser.Add (x,y) -> Peano.( + ) (eval x) (eval y)
+    | Parser.Sub (x,y) -> Peano.( - ) (eval x) (eval y)
+    | Parser.Mod (x,y) -> Peano.( % ) (eval x) (eval y)
+    | Parser.Mul (x,y) -> Peano.( * ) (eval x) (eval y)
+    | Parser.Div (x,y) -> Peano.( / ) (eval x) (eval y)
+    | Parser.Exp (x,y) -> Peano.( ^ ) (eval x) (eval y)
 
 let rec loop () =
     print_string "peano> ";
     let input = read_line () in
     let ast = input |> Parser.init |> Parser.parse_expr.run in
     let _ = match ast with 
-        | Ok (ast, _) -> eval ast |> Peano.show |> Printf.printf "= %d\n"
-        | Error e -> Printf.printf "Error: %s @ %d" e.desc e.pos;
+        | Ok (ast, _) -> 
+            let peano_result = eval ast in
+            let bracketed_repr = Parser.show ast in
+            Printf.printf "%s\n= %d\n" 
+                bracketed_repr 
+                (Peano.to_int peano_result)
+        | Error e -> Printf.printf 
+            "Error: %s @ %d" 
+            e.desc 
+            e.location;
     in
     loop ()
 
